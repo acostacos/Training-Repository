ce451097cf4ef4fd5ecc790552993c356cdc844e
Carlo Acosta <acostacarlom@gmail.com>
2021-01-21 21:00:55 +0800

    Alien Invasion - Day 3: Moving ship and shooting bullets

--- Python/Alien Invasion Game/alien_invasion.py
+++ Python/Alien Invasion Game/alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -11,14 +12,20 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     #Initialize ship
-    ship = Ship(screen)
+    ship = Ship(screen, ai_settings)
+    # Initialize group to store bullets
+    bullets = Group()
 
     #Start the main loop for the game
     while True:
         #Check for game events
-        gf.check_events()
+        gf.check_events(ai_settings, screen, ship, bullets)
+
+        #Update ship and bullet status
+        ship.update()
+        gf.update_bullets(bullets)
 
         #Update game screen
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
