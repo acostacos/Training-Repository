a8bbfebe4ee0ccb01eaa0f5578fbc9e28a98a71c
Carlo Acosta <acostacarlom@gmail.com>
2021-01-24 12:16:56 +0800

    Alien Invasion - Day 6: Added replay and increase difficulty features

--- Python/Alien Invasion Game/alien_invasion.py
+++ Python/Alien Invasion Game/alien_invasion.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame.sprite import Group
 from settings import Settings
+from game_stats import GameStats
+from button import Button
 from ship import Ship
 import game_functions as gf
 
@@ -10,6 +12,12 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    #Make the Play button
+    play_button = Button(ai_settings, screen, "Play")
+
+    #Create an instance to store game statistics
+    stats = GameStats(ai_settings)
 
     #Initialize ship, group of bullets, and group of aliens
     ship = Ship(screen, ai_settings)
@@ -22,14 +30,15 @@ def run_game():
     #Start the main loop for the game
     while True:
         #Check for game events
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
 
-        #Update ship, bullet, and alien status
-        ship.update()
-        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-        gf.update_aliens(ai_settings, ship, aliens)
+        if stats.game_active:
+            #Update ship, bullet, and alien status
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
 
         #Update game screen
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 
 run_game()
