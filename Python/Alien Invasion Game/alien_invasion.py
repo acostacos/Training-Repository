6bcf119c59db44e6ddaf3622d956114c767602ff
Carlo Acosta <acostacarlom@gmail.com>
2021-01-25 21:41:37 +0800

    Alien Invasion - Day 7: Added scoreboard and lives. Finished game tutorial

--- Python/Alien Invasion Game/alien_invasion.py
+++ Python/Alien Invasion Game/alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 import game_functions as gf
@@ -16,8 +17,9 @@ def run_game():
     #Make the Play button
     play_button = Button(ai_settings, screen, "Play")
 
-    #Create an instance to store game statistics
+    #Create an instance to store game statistics and create scoreboard
     stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
 
     #Initialize ship, group of bullets, and group of aliens
     ship = Ship(screen, ai_settings)
@@ -30,15 +32,15 @@ def run_game():
     #Start the main loop for the game
     while True:
         #Check for game events
-        gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
 
         if stats.game_active:
             #Update ship, bullet, and alien status
             ship.update()
-            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, sb, screen, ship, aliens, bullets)
 
         #Update game screen
-        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
 
 run_game()
--- Python/Alien Invasion Game/game_stats.py
+++ Python/Alien Invasion Game/game_stats.py
@@ -4,12 +4,15 @@ class GameStats():
     def __init__(self, ai_settings):
         """Initialize statistics"""
         self.ai_settings = ai_settings
+        self.high_score = 0
         self.reset_stats()
 
-        # Start Alien Invasion in an inactive status
+        #Start Alien Invasion in an inactive status
         self.game_active = False
 
     def reset_stats(self):
         """Initialize statistics that can change during the game"""
         self.ships_left = self.ai_settings.ship_limit
+        self.score = 0
+        self.level = 1
 
