745a672451fe4c6b3fe59b6002eec47ff5b91ff3
acostacos <acostacarlom@gmail.com>
2021-07-03 10:48:54 +0800

    Started working on Scott Lilly C# RPG Game

---
+++ Web Development/Javascript/Javascript Understanding The Wierd Parts/H3_strictmode/app.js
@@ -0,0 +1,20 @@
+// Activate new strict mode
+// Use it at the start of the code or start of the function
+// "use strict";
+
+function logNewPerson(){
+    "use strict";
+
+    var person2;
+    persom2 = {};
+    console.log(persom2);
+}
+
+var person;
+
+// must define a variable to be able to use it
+persom = {};
+console.log(persom);
+logNewPerson();
+
+// Not every Javascript engine runs use strict in the same way
